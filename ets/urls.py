6e45c66e067f3851ac03f85341900086fb8ddd95
isaac2000phiri <isaacisaacp@paylinkzm.com>
2024-05-20 21:13:18 -0700

    morning

--- ets/urls.py
+++ ets/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +11,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls'))
 
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
+
